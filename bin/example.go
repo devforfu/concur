3e2cd61e1cf581ee51576712a35ea84207ba0cd5
devforfu <developer.z@outlook.com>
2019-03-08 11:06:40 +0500

    Closing channel to deal with deadlock

--- bin/example.go
+++ bin/example.go
@@ -1,1 +1,21 @@
 package main
+
+func main() {
+    worklist := make(chan []string)
+    items := []string{"first", "second", "third"}
+
+    go func() {
+        worklist <- items
+        close(worklist)
+    }()
+
+    seen := make(map[string]bool)
+    for list := range worklist {
+        for _, item := range list {
+            println(item)
+            if !seen[item] {
+                seen[item] = true
+            }
+        }
+    }
+}
