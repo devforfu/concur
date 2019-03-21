f2bec6ed2da0b40aee971e5ebadf9fccbe5270f6
devforfu <developer.z@outlook.com>
2019-03-21 21:08:59 +0500

    Memo example

---
+++ cache.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "io/ioutil"
+    "net/http"
+    "os"
+)
+
+func main() {
+    memo := NewMemo(httpGetBody)
+    for url := range incomingURLs() {
+
+    }
+}
+
+func incomingURLs() <-chan string {
+    ch := make(chan string)
+    go func() {
+        for _, url := range os.Args[1:] {
+            ch <- url
+        }
+    }()
+    return ch
+}
+
+func httpGetBody(url string) (interface{}, error) {
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+    return ioutil.ReadAll(resp.Body)
+}
+
+type Memo struct {
+    f Func
+    cache map[string]result
+}
+
+type Func func(key string) (interface{}, error)
+
+type result struct {
+    value interface{}
+    err error
+}
+
+func NewMemo(f Func) *Memo {
+    return &Memo{f: f, cache: make(map[string]result)}
+}
+
+func (memo *Memo) Get(key string) (interface{}, error) {
+    res, ok := memo.cache[key]
+    if !ok {
+        res.value, res.err = memo.f(key)
+        memo.cache[key] = res
+    }
+    return res.value, res.err
+}
