840fc05318396cccf4b640d9a22de9ed21c4b0ec
devforfu <developer.z@outlook.com>
2019-03-30 23:32:05 +0500

    Concurrent cache

--- memo/memo.go
+++ memo/memo.go
@@ -1,8 +1,11 @@
 package memo
 
+import "sync"
+
 type Memo struct {
     f Func
-    cache map[string]result
+    mu sync.Mutex
+    cache map[string]*entry
 }
 
 type Func func(key string) (interface{}, error)
@@ -12,15 +15,27 @@ type result struct {
     err error
 }
 
+type entry struct {
+    res result
+    ready chan struct{}
+}
+
 func New(f Func) *Memo {
-    return &Memo{f: f, cache: make(map[string]result)}
+    return &Memo{f: f, cache: make(map[string]*entry)}
 }
 
 func (memo *Memo) Get(key string) (interface{}, error) {
-    res, ok := memo.cache[key]
-    if !ok {
-        res.value, res.err = memo.f(key)
-        memo.cache[key] = res
+    memo.mu.Lock()
+    e := memo.cache[key]
+    if e == nil {
+        e = &entry{ready: make(chan struct{})}
+        memo.cache[key] = e
+        memo.mu.Unlock()
+        e.res.value, e.res.err = memo.f(key)
+        close(e.ready)
+    } else {
+        memo.mu.Unlock()
+        <- e.ready
     }
-    return res.value, res.err
+    return e.res.value, e.res.err
 }
